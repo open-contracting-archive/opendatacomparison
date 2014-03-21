840b29542f829e215d3dc0369399fb31ee2671e7
Sarah Bird <sarah@bonvaya.com>
2014-03-21 14:40:45 -0700

    Add datamap for notes and file types

--- django/website/datamap/models.py
+++ django/website/datamap/models.py
@@ -3,15 +3,30 @@ from django.db.models import (
     CharField,
     TextField,
     ForeignKey,
+    ManyToManyField,
 )
-from package.models import Package
+from package.models import Package, Format
 from core.models import BaseModel
 from international.models import languages
 
 
-class Field(BaseModel):
+class Datamap(BaseModel):
     dataset = ForeignKey(Package)
+    notes = TextField(_('Notes'), null=True, blank=True)
+    format = ForeignKey(Format)
+
+
+class Concept(BaseModel):
+    name = CharField(_('Name'), max_length=100, unique=True)
+    description = TextField(_('Description'))
+    parent = ForeignKey('self', null=True, blank=True)
+
+
+class Field(BaseModel):
+    datamap = ForeignKey(Datamap)
     fieldname = CharField(_('Field Name'), max_length=100)
+    concept = ForeignKey(Concept, null=True, blank=True)
+    mapsto = ManyToManyField('self', null=True, blank=True)
     datatype = CharField(_('Type'),
                          max_length=30,
                          choices=(
@@ -23,6 +38,9 @@ class Field(BaseModel):
                              ('SingleSelect', _('Single Select')),
                              ('MultiSelect', _('MultiSelect')),
                          ))
+
+    class Meta:
+        unique_together = ('datamap', 'fieldname')
 
 
 class TranslatedField(BaseModel):
