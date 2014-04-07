bb811336840b8362c34055266837c18c33b98fff
Sarah Bird <sarah@bonvaya.com>
2014-04-07 14:06:59 -0700

    Refactor homepage tests

--- django/website/homepage/tests/test_views.py
+++ django/website/homepage/tests/test_views.py
@@ -4,17 +4,16 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import RequestFactory
 
+from core.tests.data import OpenComparisonTestCase
+from grid.tests.data import GridTestCase
 from grid.models import Grid
 from homepage.models import Dpotw, Gotw
 from package.models import Package, Category
 
 from homepage.views import error_500_view
-from homepage.tests import data
 
 
-class FunctionalHomepageTest(TestCase):
-    def setUp(self):
-        data.load()
+class FunctionalHomepageTest(GridTestCase):
 
     def test_homepage_view(self):
         url = reverse('home')
@@ -24,19 +23,23 @@ class FunctionalHomepageTest(TestCase):
 
         for p in Package.objects.all():
             self.assertContains(response, p.title)
-            self.assertContains(response, p.repo_description)
+            self.assertContains(response, p.description)
 
-        self.assertEquals(response.context['package_count'], Package.objects.count())
+        self.assertEquals(response.context['package_count'],
+                          Package.objects.count())
 
-    def test_categories_on_homepage(self):
+    def test_packages_on_homepage(self):
+        """
+        Latest packages should be listed on the homepage
+        """
         url = reverse('home')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'homepage.html')
 
-        for c in Category.objects.all():
-            self.assertContains(response, c.title_plural)
-            self.assertContains(response, c.description)
+        for p in Package.objects.all():
+            self.assertContains(response, p.title)
+            self.assertContains(response, p.description)
 
     def test_items_of_the_week(self):
         url = reverse('home')
@@ -47,9 +50,13 @@ class FunctionalHomepageTest(TestCase):
         p = Package.objects.all()[0]
         g = Grid.objects.all()[0]
 
-        d_live = Dpotw.objects.create(package=p, start_date=yesterday, end_date=tomorrow)
+        d_live = Dpotw.objects.create(package=p,
+                                      start_date=yesterday,
+                                      end_date=tomorrow)
 
-        g_live = Gotw.objects.create(grid=g, start_date=yesterday, end_date=tomorrow)
+        g_live = Gotw.objects.create(grid=g,
+                                     start_date=yesterday,
+                                     end_date=tomorrow)
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -58,9 +65,7 @@ class FunctionalHomepageTest(TestCase):
         self.assertContains(response, g_live.grid.title)
 
 
-class FunctionalHomepageTestWithoutPackages(TestCase):
-    def setUp(self):
-        data.load()
+class FunctionalHomepageTestWithoutPackages(OpenComparisonTestCase):
 
     def test_homepage_view(self):
         Package.objects.all().delete()
@@ -77,5 +82,5 @@ class TestErrorPages(TestCase):
         self.assertEquals(r.status_code, 404)
 
     def test_500_test(self):
-        r = error_500_view(RequestFactory())
+        r = error_500_view(RequestFactory().get('/'))
         self.assertEquals(r.status_code, 500)
