e6eba7a12cd5e92d8520b1df336107ddf965346b
Sarah Bird <sarah@bonvaya.com>
2014-04-07 11:44:03 -0700

    Get profile test_views working

--- django/website/core/tests/data.py
+++ django/website/core/tests/data.py
@@ -1,92 +1,91 @@
 from django.contrib.auth.models import User
+from django.test import TestCase
 
 from package.models import Category, Package
 
 STOCK_PASSWORD = "stock_password"
 
 
-def make():
+class OpenComparisonTestCase(TestCase):
 
-    create_users()
+    def setUp(self):
+        self.make()
 
-    category, created = Category.objects.get_or_create(
-        title="App",
-        slug="apps",
-        description="Small components used to build projects."
-    )
-    category.save()
+    def make(self):
 
-    package, created = Package.objects.get_or_create(
-        category = category,
-        participants = "malcomt,jacobian",
-        repo_description = "Increase your testing ability with this steroid free supplement.",
-        repo_url = "https://github.com/pydanny/django-la-facebook",
-        slug = "testability",
-        title="Testability"
-    )
-    package.save()
-    package, created = Package.objects.get_or_create(
-        category = category,
-        participants = "thetestman",
-        repo_description = "Test everything under the sun with one command!",
-        repo_url = "https://github.com/pydanny/django-uni-form",
-        slug = "supertester",
-        title="Supertester"
-    )
-    package.save()
-    package, created = Package.objects.get_or_create(
-        category = category,
-        participants = "pydanny",
-        repo_description = "Make testing as painless as frozen yogurt.",
-        repo_url = "https://github.com/opencomparison/opencomparison",
-        slug = "serious-testing",
-        title="Serious Testing"
-    )
-    package.save()    
-    package, created = Package.objects.get_or_create(
-        category = category,
-        participants = "pydanny",
-        repo_description = "Yet another test package, with no grid affiliation.",
-        repo_url = "https://github.com/djangopackages/djangopackages",
-        slug = "another-test",
-        title="Another Test"
-    )
-    package.save()
+        self.create_users()
 
+        category, created = Category.objects.get_or_create(
+            title="App",
+            slug="apps",
+            description="Small components used to build projects."
+        )
+        package, created = Package.objects.get_or_create(
+            category=category,
+            description="Increase your testing ability with this steroid free supplement.",  # nopep8
+            url="https://github.com/pydanny/django-la-facebook",
+            slug="testability",
+            title="Testability",
+            machine_readable=False,
+        )
+        package, created = Package.objects.get_or_create(
+            category=category,
+            description="Test everything under the sun with one command!",
+            url="https://github.com/pydanny/django-uni-form",
+            slug="supertester",
+            title="Supertester",
+            machine_readable=False,
+        )
+        package, created = Package.objects.get_or_create(
+            category=category,
+            description="Make testing as painless as frozen yogurt.",
+            url="https://github.com/opencomparison/opencomparison",
+            slug="serious-testing",
+            title="Serious Testing",
+            machine_readable=False,
+        )
+        package, created = Package.objects.get_or_create(
+            category=category,
+            description="Yet another test package, with no grid affiliation.",  # nopep8
+            url="https://github.com/djangopackages/djangopackages",
+            slug="another-test",
+            title="Another Test",
+            machine_readable=False,
+        )
 
-def create_users():
+    def create_users(self):
 
-    user = User.objects.create_user(
-        username="user",
-        password=STOCK_PASSWORD,
-        email="user@example.com"
-    )
-    user.is_active = True
-    user.save()
+        user = User.objects.create_user(
+            username="user",
+            password=STOCK_PASSWORD,
+            email="user@example.com"
+        )
+        user.is_active = True
+        user.save()
 
-    user = User.objects.create_user(
-        username="cleaner",
-        password="cleaner",
-        email="cleaner@example.com"
-    )
-    user.is_active = True
-    user.save()
+        user = User.objects.create_user(
+            username="cleaner",
+            password="cleaner",
+            email="cleaner@example.com"
+        )
+        user.is_active = True
+        user.save()
 
-    user = User.objects.create_user(
-        username="staff",
-        password="staff",
-        email="staff@example.com"
-    )
-    user.is_active = True
-    user.is_staff = True
-    user.save()
+        user = User.objects.create_user(
+            username="staff",
+            password="staff",
+            email="staff@example.com"
+        )
+        user.is_active = True
+        user.is_staff = True
+        user.save()
 
-    user = User.objects.create_user(
-        username="admin",
-        password="admin",
-        email="admin@example.com"
-    )
-    user.is_active = True
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
+        user = User.objects.create_user(
+            username="admin",
+            password="admin",
+            email="admin@example.com"
+        )
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
--- django/website/profiles/tests/test_views.py
+++ django/website/profiles/tests/test_views.py
@@ -1,49 +1,42 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.test import TestCase
 
-from core.tests.data import create_users, STOCK_PASSWORD
+from core.tests.data import OpenComparisonTestCase, STOCK_PASSWORD
 from profiles.models import Profile
 
 
-class TestProfile(TestCase):
+class TestProfile(OpenComparisonTestCase):
 
     def setUp(self):
         super(TestProfile, self).setUp()
-        create_users()
         self.user = User.objects.get(username="user")
-        self.profile = Profile.objects.create(
-            github_account="user",
-            user=self.user,
-        )
+        self.profile, created = Profile.objects.get_or_create(user=self.user)
 
     def test_view(self):
-        self.assertTrue(self.client.login(username=self.user.username, password=STOCK_PASSWORD))
-        url = reverse('profile_detail', kwargs={'github_account': self.profile.github_account})
+        self.assertTrue(self.client.login(username=self.user.username,
+                                          password=STOCK_PASSWORD))
+        url = reverse('profile_detail', args=(self.user.username,))
         response = self.client.get(url)
         self.assertContains(response, "Profile for user")
 
     def test_view_not_loggedin(self):
-        url = reverse('profile_detail', kwargs={'github_account': self.profile.github_account})
+        url = reverse('profile_detail', args=(self.user.username,))
         response = self.client.get(url)
         self.assertContains(response, "Profile for user")
 
     def test_edit(self):
-        self.assertTrue(self.client.login(username=self.user.username, password=STOCK_PASSWORD))
-
+        self.assertTrue(self.client.login(username=self.user.username,
+                                          password=STOCK_PASSWORD))
         # give me a view
-        url = reverse('profile_edit')
+        url = reverse('profile_edit', args=(self.user.username,))
         response = self.client.get(url)
-        stuff = """<input id="id_bitbucket_url" type="text" class="textInput textinput" name="bitbucket_url" maxlength="100" />"""
-        stuff = """<input id="id_bitbucket_url" type="text" class="textinput textInput form-control" name="bitbucket_url" maxlength="100" />"""
+        stuff = """<input class="urlinput form-control" id="id_url" maxlength="200" name="url" type="url" />"""  # nopep8
         self.assertContains(response, stuff)
 
         # submit some content
-        data = {
-            'bitbucket_url': 'zerg',
-            }
+        data = {'url': 'http://www.zerg.com'}
         response = self.client.post(url, data, follow=True)
         self.assertContains(response, "Profile for user")
         p = Profile.objects.get(user=self.user)
-        self.assertEquals(p.bitbucket_url, "zerg")
+        self.assertEquals(p.url, "http://www.zerg.com/")
