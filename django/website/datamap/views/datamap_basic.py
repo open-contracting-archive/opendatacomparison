e4a6d5b9198c03d5f542dc53982b1b93cccc3ca0
Sarah Bird <sarah@bonvaya.com>
2014-05-14 23:11:27 -0700

    Add visualization to list of all datamaps

--- django/website/datamap/views/datamap_basic.py
+++ django/website/datamap/views/datamap_basic.py
@@ -27,17 +27,45 @@ from bokeh.plotting import (
 )
 from bokeh.objects import HoverTool
 from collections import OrderedDict
-from braces.views import LoginRequiredMixin, JSONResponseMixin
+from braces.views import LoginRequiredMixin
 from extra_views import (
     InlineFormSet
 )
-from datamap.models import Datamap, Field
+from datamap.models import Datamap, Field, Concept
 from datamap.forms import FieldForm
 from package.models import Package
 
 
 class DatamapListView(ListView):
     model = Datamap
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DatamapListView,
+                        self).get_context_data(*args, **kwargs)
+        context['visual'] = self.build_visual_map()
+        return context
+
+    def build_visual_map(self):
+        # Make the lists for the plot
+        y = []
+        x = []
+        radii = []
+        fields_in_concept = []
+        datamaps = Datamap.objects.all()
+        for datamap in datamaps:
+            concept_dict = get_empty_concept_dict()
+            fields = datamap.fields.all()
+            for field in fields:
+                concept = field.concept
+                concept_dict[concept.name].append(field.fieldname)
+
+            for concept, fields in concept_dict.iteritems():
+                y.append(concept)
+                x.append(get_x_label(datamap))
+                radii.append(len(fields) * 5)
+                fields_in_concept.append(', '.join(fields))
+        return build_plot(x, y, radii, fields_in_concept,
+                          datamaps, concept_dict.keys())
 
 
 class FieldInline(InlineFormSet):
@@ -124,6 +152,85 @@ class DatamapView(DetailView):
         return context
 
     def build_visual_map(self):
+        concept_dict = get_empty_concept_dict()
+        fields = self.object.fields.all()
+        for field in fields:
+            concept = field.concept
+            concept_dict[concept.name].append(field.fieldname)
+
+        # Make the lists for the plot
+        y = []
+        x = []
+        radii = []
+        fields_in_concept = []
+        for concept, fields in concept_dict.iteritems():
+            y.append(concept)
+            x.append(get_x_label(self.object))
+            radii.append(len(fields) * 5)
+            fields_in_concept.append(', '.join(fields))
+        return build_plot(x, y, radii, fields_in_concept,
+                          [self.object], concept_dict.keys())
+
+
+class BokehJS(View):
+    def get(self, request, **kwargs):
+        try:
+            file_object = open('%s/%s.embed.js' % (settings.BOKEH_EMBED_JS_DIR,
+                                                   kwargs.get('uuid')), 'rb')
+        except (ValueError, IOError):
+            return HttpResponseNotFound()
+        return HttpResponse(file_object)
+
+
+def build_plot(x, y, radii, fields_in_concept, datamaps, concepts):
+    source = ColumnDataSource(
+        data=dict(
+            x=x,
+            y=y,
+            radii=radii,
+            fields_in_concept=fields_in_concept,
+        )
+    )
+    output_file('')
+    hold()
+    figure()
+    plot_properties = {
+        'title': None,
+        'tools': "hover",
+        'x_range': [get_x_label(datamap) for datamap in datamaps],
+        'y_range': concepts,
+    }
+
+    rect(x, y, 1, 1,
+        source=source,
+        color='white', # put in background
+        **plot_properties)
+
+    circle('x', 'y',
+        source=source,
+        size='radii',
+        color='black',
+        **plot_properties)
+
+    grid().grid_line_color = None
+
+    hover = [t for t in curplot().tools if isinstance(t, HoverTool)][0]
+
+    hover.tooltips = OrderedDict([
+        ("Bucket", "@y"),
+        ("Fields", "@fields_in_concept"),
+    ])
+
+    return curplot().create_html_snippet(
+        static_path=settings.STATIC_URL,
+        embed_save_loc=settings.BOKEH_EMBED_JS_DIR,
+        embed_base_url=reverse('bokeh'),
+    )
+
+
+def get_empty_concept_dict():
+    """
+    Returns a list of concepts (not yet in the order we want)
         concepts = [
                 "SYSTEM",
                 "DOCUMENT",
@@ -139,77 +246,14 @@ class DatamapView(DetailView):
                 "CONTRACT FEATURES",
                 "ADD ON"
         ]
-        fields = self.object.fields.all()
-        empty_list = ([] for concept in concepts)
-        concept_dict = OrderedDict(zip(concepts, empty_list))
-        for field in fields:
-            concept = field.concept
-            concept_dict[concept.name].append(field.fieldname)
+    """
+    concepts = \
+        Concept.objects.all().values_list('name', flat=True).order_by('name')
+    empty_list = ([] for concept in concepts)
+    concept_dict = OrderedDict(zip(concepts, empty_list))
+    return concept_dict
 
-        # Make the lists for the plot
-        y = []
-        x = []
-        radii = []
-        fields_in_concept = []
-        for concept, fields in concept_dict.iteritems():
-            y.append(concept)
-            x.append(self._get_x_label(self.object))
-            radii.append(len(fields)*5)
-            fields_in_concept.append(', '.join(fields))
 
-        source = ColumnDataSource(
-            data=dict(
-                x=x,
-                y=y,
-                radii=radii,
-                fields_in_concept=fields_in_concept,
-            )
-        )
-        output_file('')
-        hold()
-        figure()
-        plot_properties = {
-            'title': None,
-            'tools': "hover",
-            'x_range': [self._get_x_label(self.object)],
-            'y_range': concepts,
-        }
-
-        rect(x, y, 1, 1,
-            source=source,
-            color='white', # put in background
-            **plot_properties)
-
-        circle('x', 'y',
-            source=source,
-            size='radii',
-            color='black',
-            **plot_properties)
-
-        grid().grid_line_color = None
-
-        hover = [t for t in curplot().tools if isinstance(t, HoverTool)][0]
-
-        hover.tooltips = OrderedDict([
-            ("Bucket", "@y"),
-            ("Fields", "@fields_in_concept"),
-        ])
-
-        return curplot().create_html_snippet(
-            static_path=settings.STATIC_URL,
-            embed_save_loc=settings.BOKEH_EMBED_JS_DIR,
-            embed_base_url=reverse('bokeh'),
-        )
-
-    def _get_x_label(self, datamap):
-        return '%s - %s' % (datamap.dataset.publisher.country,
-                            datamap.dataset.publisher.name)
-
-class BokehJS(View):
-    def get(self, request, **kwargs):
-        try:
-            file_object = open('%s/%s.embed.js' % (settings.BOKEH_EMBED_JS_DIR,
-                                                   kwargs.get('uuid')), 'rb')
-        except (ValueError, IOError):
-            return HttpResponseNotFound()
-        return HttpResponse(file_object)
+def get_x_label(datamap):
+    return '%s - %s' % (datamap.dataset.publisher.country,
+                        datamap.dataset.publisher.name)
